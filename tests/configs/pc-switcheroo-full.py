cb3383099b9f6be464aa3bf63ce47201344be5de
Joe Rocklin <joe.rocklin@gmail.com>
2013-04-30 21:04:47 -0400

    Update from HG changeset 9680:217bdd9a3ad9

--- tests/configs/pc-switcheroo-full.py
+++ tests/configs/pc-switcheroo-full.py
@@ -1,5 +1,15 @@
-# Copyright (c) 2007 The Regents of The University of Michigan
+# Copyright (c) 2012 ARM Limited
+# Copyright (c) 2013 Mark D. Hill and David A. Wood
 # All rights reserved.
+#
+# The license below extends only to copyright in the software and shall
+# not be construed as granting a license to any other intellectual
+# property including but not limited to intellectual property relating
+# to a hardware implementation of the functionality of the software
+# licensed hereunder.  You may use the software subject to the license
+# terms below provided that you ensure that this notice is replicated
+# unmodified and in its entirety in all distributions of the software,
+# modified or unmodified, in source code or in binary form.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
@@ -24,20 +34,17 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# Authors: Ali Saidi
+# Authors: Andreas Sandberg
+#          Nilay Vaish
 
-import m5
 from m5.objects import *
-m5.util.addToPath('../configs/common')
-import FSConfig
+from x86_generic import *
+import switcheroo
 
-cpu = AtomicSimpleCPU(cpu_id=0)
-system = FSConfig.makeSparcSystem('atomic', SimpleDDR3)
-system.cpu = cpu
-# create the interrupt controller
-cpu.createInterruptController()
-cpu.connectAllPorts(system.membus)
+root = LinuxX86FSSwitcheroo(
+    cpu_classes=(AtomicSimpleCPU, TimingSimpleCPU, DerivO3CPU)
+    ).create_root()
 
-root = Root(full_system=True, system=system)
-
-m5.ticks.setGlobalFrequency('2GHz')
+# Setup a custom test method that uses the switcheroo tester that
+# switches between CPU models.
+run_test = switcheroo.run_test
